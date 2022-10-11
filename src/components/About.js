81f522fd3dc13cfaacdb84334b46a4f05a195337
Lapeee <lauri.ilkka@gmail.com>
2022-10-11 14:18:57 +0300

    spa example

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h3>About us</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+  )
+}
---
+++ src/components/ContactUs.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function ContactUs() {
+  return (
+    <div>
+      <h3>Ota Yhteyttä</h3>
+      <ul>
+        <li>
+          Puhelin: 04040404040
+        </li>
+        <li>
+          sähköposti: asdasd@gmail.com
+        </li>
+        <li>
+          Osoite: jokukatu 9
+        </li>
+      </ul>
+    </div>
+  )
+}
